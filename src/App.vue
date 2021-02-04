e602ffa4c64a56268ceb31ed7517f023094e507e
olivermanzi <olivermanzi.dev@gmail.com>
2021-02-04 23:37:35 +0100

    sets layout goals

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <b-row align-h="center">
+      <b-col cols="auto">
+        So-What-Now?
+      </b-col>
+    </b-row>
+    <b-row class="my-5">
+      <b-col cols="12">
+        <landing />
+      </b-col>
+    </b-row>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+	import landingVue from "./components/landing.vue";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	export default {
+		name: "App",
+		components: {
+			"landing":landingVue
+		}
+	};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/landing.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+		<b-row align-h="center">
+			<b-col cols="auto">
+				cards
+			</b-col>
+		</b-row>
+		<b-row align-h="center">
+			<b-col cols="auto">
+				buttons
+			</b-col>
+		</b-row>
+	</div>
+</template>
+
+<script>
+	import { mapGetters } from "vuex";
+	export default {
+		name: "Landing",
+		computed: {
+			...mapGetters({
+				sdg:"getSdg"
+			})
+		}
+	};
+</script>
